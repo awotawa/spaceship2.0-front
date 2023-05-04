e45137bf517f396672cd1147a6d9f4902a6990e1
Alexandre Wotawa <awotawa@gmail.com>
2023-05-04 16:49:32 +0200

    feat: call in App

--- spaceship-2/src/App.tsx
+++ spaceship-2/src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  getAstronauts,
+  addAstronaut,
+  updateAstronaut,
+  deleteAstronaut,
+} from "./API";
+import AddAstronaut from "./components/AddAstronaut";
+import AstronautItem from "./components/AstronautItem";
 
-function App() {
+const App: React.FC = () => {
+  const [astronauts, setAstronauts] = useState<IAstronaut[]>([]);
+
+  useEffect(() => {
+    fetchAstronauts();
+  }, []);
+
+  const fetchAstronauts = (): void => {
+    getAstronauts()
+      .then(({ data: { astronauts } }: IAstronaut[] | any) =>
+        setAstronauts(astronauts)
+      )
+      .catch((err: Error) => console.log(err));
+  };
+
+  const handleSaveAstronaut = (
+    e: React.FormEvent,
+    formData: IAstronaut
+  ): void => {
+    e.preventDefault();
+    addAstronaut(formData)
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error! Astronaut not saved");
+        }
+        setAstronauts(data.astronauts);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleUpdateAstronaut = (astronaut: IAstronaut): void => {
+    updateAstronaut(astronaut)
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error! Astronaut not updated");
+        }
+        setAstronauts(data.astronauts);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleDeleteAstronaut = (_id: string): void => {
+    deleteAstronaut(_id)
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error("Error! Astronaut not deleted");
+        }
+        setAstronauts(data.astronauts);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>My Astronauts</h1>
+      <AddAstronaut saveAstronaut={handleSaveAstronaut} />
+      {astronauts.map((astronaut: IAstronaut) => (
+        <AstronautItem
+          key={astronaut._id}
+          updateAstronaut={handleUpdateAstronaut}
+          deleteAstronaut={handleDeleteAstronaut}
+          astronaut={astronaut}
+        />
+      ))}
+    </main>
   );
-}
+};
 
 export default App;
